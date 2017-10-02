ba4568a8e3ad8860aad27f01dd13ff823bda463f
Daria Kovtun <dashakovtun27@gmail.com>
2017-10-02 19:25:59 +0300

    Add homework for 03.10.2017

--- List/src/ru/itis/ArrayList.java
+++ List/src/ru/itis/ArrayList.java
@@ -10,8 +10,40 @@ public class ArrayList {
             count++;
         }
         else {
-            System.err.println("Нет места.");
+            array = longerArray();
+            addToEnd(element);
         }
+    }
+
+    void deleteNumber(int index) {
+        for (int i = index; i < array.length-1; i++) {
+            array[i] = array[i+1];
+        }
+        count -= 1;
+    }
+
+    void insertNumber(int index, int number) {
+        if (count < array.length) {
+            if (index <= count) {
+                for (int i = count-1; i > index; i--) {
+                    array[i] = array[i-1];
+                }
+                array[index] = number;
+                count += 1;
+            }
+        }
+        else {
+            array = longerArray();
+            insertNumber(index, number);
+        }
+    }
+
+    private int[] longerArray() {
+        int arrayCopy[] = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
+            arrayCopy[i] = array[i];
+        }
+        return arrayCopy;
     }
 
     void showList() {
--- List/src/ru/itis/Main.java
+++ List/src/ru/itis/Main.java
@@ -1,13 +1,53 @@
 package ru.itis;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList list = new ArrayList();
-        list.addToEnd(7);
-        list.addToEnd(8);
-        list.addToEnd(9);
 
-        list.showList();
+        while (true) {
+            System.out.println("1. Создать массив");
+            System.out.println("2. Добавить число в конец массива");
+            System.out.println("3. Удалить число из массива");
+            System.out.println("4. Вставить число в массив");
+            System.out.println("5. Вывести массив на экран");
+            System.out.println("6. Выход");
+
+            int c = scanner.nextInt();    //num command
+
+            if (c == 1) {
+                int n = scanner.nextInt();
+                for (int i = 0; i < n; i++) {
+                    list.addToEnd(scanner.nextInt());
+                }
+            }
+
+            if (c == 2) {
+                list.addToEnd(scanner.nextInt());
+            }
+
+            if (c == 3) {
+                list.deleteNumber(scanner.nextInt());
+            }
+
+            if (c == 4) {
+                int index = scanner.nextInt();
+                int number = scanner.nextInt();
+                list.insertNumber(index,number);
+            }
+
+            if (c == 5) {
+                list.showList();
+            }
+
+            if (c == 6) {
+                System.exit(0);
+            }
+        }
+
+//        System.out.println("I don't fly to Rome with coffee stains!");
     }
 }
